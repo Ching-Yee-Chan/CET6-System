5467c2cdb195af5b8d36266108a8e353720bf28a
NKULYX <1078346719@qq.com>
2023-07-01 09:55:08 +0800

    add question in backend

---
+++ cet6/src/main/java/org/example/controller/exam/QuestionController.java
@@ -0,0 +1,99 @@
+package org.example.controller.exam;
+
+import org.example.common.Result;
+import org.example.common.SuperController;
+import org.example.pojo.entity.Question;
+import org.example.service.QuestionService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * (Question)表控制层
+ *
+ * @author Yuxuan
+ * @since 2023-06-26 17:33:14
+ */
+@RestController
+@RequestMapping("question")
+public class QuestionController extends SuperController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private QuestionService questionService;
+
+    /**
+     * 分页查询
+     *
+     * @param question 筛选条件
+     * @param pageNum  当前页
+     * @param pageSize 每页显示数量
+     * @return 查询结果
+     */
+    @PostMapping("/{pageNum}/{pageSize}")
+    public Result queryByPage(@RequestBody Question question,
+                              @PathVariable("pageNum") Integer pageNum,
+                              @PathVariable("pageSize") Integer pageSize) {
+        return success(this.questionService.queryByPage(question, PageRequest.of(pageNum - 1, pageSize)));
+    }
+
+    /**
+     * 查询全部数据
+     *
+     * @param question  筛选条件
+     * @return  查询结果
+     */
+    @PostMapping("/all")
+    public Result queryAll(@RequestBody Question question) {
+        return success(this.questionService.queryAll(question));
+    }
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<Question> queryById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(this.questionService.queryById(id));
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param question 实体
+     * @return 新增结果
+     */
+    @PostMapping
+    public Result add(@RequestBody Question question) {
+        return success(this.questionService.insert(question));
+    }
+
+    /**
+     * 编辑数据
+     *
+     * @param question 实体
+     * @return 编辑结果
+     */
+    @PutMapping
+    public Result edit(@RequestBody Question question) {
+        return success(this.questionService.update(question));
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param id 主键
+     * @return 删除是否成功
+     */
+    @DeleteMapping("{id}")
+    public Result deleteById(@PathVariable("id") Integer id) {
+        return success(this.questionService.deleteById(id));
+    }
+
+}
+
---
+++ cet6/src/main/java/org/example/service/QuestionService.java
@@ -0,0 +1,64 @@
+package org.example.service;
+
+import org.example.pojo.entity.Question;
+import com.github.pagehelper.PageInfo;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * (Question)表服务接口
+ *
+ * @author Yuxuan
+ * @since 2023-06-26 17:33:15
+ */
+public interface QuestionService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param questionid 主键
+     * @return 实例对象
+     */
+    Question queryById(Integer questionid);
+
+    /**
+     * 分页查询
+     *
+     * @param question    筛选条件
+     * @param pageRequest 分页对象
+     * @return 查询结果
+     */
+    PageInfo<Question> queryByPage(Question question, PageRequest pageRequest);
+
+    /**
+     * 查询全部数据
+     *
+     * @param question  筛选条件
+     * @return 查询结果
+     */
+    PageInfo<Question> queryAll(Question question);
+
+    /**
+     * 新增数据
+     *
+     * @param question 实例对象
+     * @return 实例对象
+     */
+    Question insert(Question question);
+
+    /**
+     * 修改数据
+     *
+     * @param question 实例对象
+     * @return 实例对象
+     */
+    Question update(Question question);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param questionid 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Integer questionid);
+
+}
